0c70048e7c5fe9952e4497f229caa065641af966
Raj <rajdama1729@gmail.com>
2024-06-30 12:17:24 +0530

    env

--- client/src/app/api/auth/[...nextauth]/route.js
+++ client/src/app/api/auth/[...nextauth]/route.js
@@ -3,9 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "85823771625-je3d2dfhou3d4brqj2q4g0q015s7mc8o.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
     }),
   ],
 });
